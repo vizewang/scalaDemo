0b78dfac93fa981854d96ebd868513fc0aa21802
weizwang <weizwang@ebay.com>
2016-01-22 14:06:33 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "scalaDemo"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ src/main/scala/com/vizewang/demo/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.vizewang.demo.scala
+
+/**
+ * Created by weizwang on 2016/1/22.
+ */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello,SBT")
+  }
+}
